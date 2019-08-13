7d6437431d5ee79e57c4a5e2d105d84b637ee7a6
Steven C <stevenc.github@gmail.com>
2019-08-13 13:02:32 -0400

    Restructured the order of the function.

--- get_proxy.py
+++ get_proxy.py
@@ -26,51 +26,53 @@ def get_proxy():
 
     for xml in file_list:
 
-        xml_fpath = os.path.join(xmlpath, xml)
-
-        tree = ET.parse(xml_fpath)
-        root = tree.getroot()
-
-        titletype = root[0][8].text
-
         if xml == '.DS_Store':
             os.remove(xmlpath + '.DS_Store')
 
-        elif titletype is not "video":
-            continue
-            # set up a move for the XML if it is an archive
         else:
-            pass
+            xml_fpath = os.path.join(xmlpath, xml)
 
-        guid_x = xml.replace("-", "")
-        guid = guid_x[24:-4]
-        n = 2
-        glist = [guid[i:i+n] for i in range(0, len(guid), n)]
+            tree = ET.parse(xml_fpath)
+            root = tree.getroot()
 
-        full_proxypath = os.path.join(proxypath, glist[2], glist[3], xml[:-4], xml[:-4] + '.mov')
+            titletype = root[0][8].text
 
-        if os.path.exists(full_proxypath) is not True:
-            proxy_err_msg = f"Proxy path does not exist. \
-            {full_proxypath}"
-            logger.error(proxy_err_msg)
+            if titletype != "video":
+                tt_info_msg = f"{xml} titletype is not 'video', skipping get_proxy."
+                logger.info(tt_info_msg)
+                pass
+                # set up a move for the XML if it is an archive
 
-        else:
-            try:
-                shutil.copy2(full_proxypath, tmp_checkin)
-                proxy_cp_msg = f"{guid} was copied to the dalet tmp."
-                logger.info(proxy_cp_msg)
+            else:
+                guid_x = xml.replace("-", "")
+                guid = guid_x[24:-4]
+                n = 2
+                glist = [guid[i:i+n] for i in range(0, len(guid), n)]
 
-                shutil.move(xml_fpath, tmp_checkin)
-                xml_mv_msg = f"{xml} was moved to the dalet tmp."
-                logger.info(xml_mv_msg)
+                full_proxypath = os.path.join(proxypath, glist[2], glist[3], xml[:-4], xml[:-4] + '.mov')
 
-            except Exception as e:
-                proxy_excp_msg = f"\n\
-                Exception raised on the Proxy copy.\n\
-                Error Message:  {str(e)} \n\
-                "
+                if os.path.exists(full_proxypath) is not True:
+                    proxy_err_msg = f"Proxy path does not exist. \
+                    {full_proxypath}"
+                    logger.error(proxy_err_msg)
 
-                logger.exception(proxy_excp_msg)
+                else:
+                    try:
+                        shutil.copy2(full_proxypath, tmp_checkin)
+                        proxy_cp_msg = f"{guid} was copied to the dalet tmp."
+                        logger.info(proxy_cp_msg)
+
+                        shutil.move(xml_fpath, tmp_checkin)
+                        xml_mv_msg = f"{xml} was moved to the dalet tmp."
+                        logger.info(xml_mv_msg)
+
+                    except Exception as e:
+                        proxy_excp_msg = f"\n\
+                        Exception raised on the Proxy copy.\n\
+                        Error Message:  {str(e)} \n\
+                        "
+
+                        logger.exception(proxy_excp_msg)
 
 
 if __name__ == '__main__':
