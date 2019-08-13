7335060c9ba548f6b0929f3d6eb79fc1151e63e1
Steven C <stevenc.github@gmail.com>
2019-08-13 17:34:30 -0400

    Added a df sort based on creation date, and changed index start/stop to xml_total.

--- create_xml.py
+++ create_xml.py
@@ -11,7 +11,7 @@ from xml.dom import minidom
 logger = logging.getLogger(__name__)
 
 
-def create_xml(cleaned_csv):
+def create_xml(cleaned_csv, xml_total):
     """
     Use the merged, parsed, cleaned, DB data to generate an XML used for file check in on the MAM.
     """
@@ -32,65 +32,72 @@ def create_xml(cleaned_csv):
         try:
             pd_reader = pd.read_csv(c_csv, header=0)
             df = pd.DataFrame(pd_reader)
+            df.sort_values(by=['CREATEDT'], ascending=False)
 
-            for index, row in df.iterrows():
+            count = 0
 
-                guid = row['GUID']
-                name = row['NAME']
-                datatapeid = row['DATATAPEID']
-                timecodein = row['TIMECODEIN']
-                folderpath = "T://DaletStorage/Video_Watch_Folder/" + row["OC_COMPONENT_NAME"]
-                traffic_code = row['TRAFFIC_CODE'].strip("=\"")
-                title_type = row['TITLETYPE']
-                framerate = row['FRAMERATE']
-                codec = row['CODEC']
-                v_width = row['V_WIDTH']
-                v_height = row['V_HEIGHT']
-                duration = row['DURATION_MS']
-                content_type = row['CONTENT_TYPE']
+            for index, row in sdf.iterrows():
 
-                os.chdir(rootpath + "_xml/")
-                xml_doc = str(guid) + '.xml'
+                if (row['XML_CREATED'] == '0'
+                    and count <= xml_total):
 
-                with open(xml_doc, mode="w", encoding='utf-8-sig') as xdoc:
+                    guid = row['GUID']
+                    name = row['NAME']
+                    datatapeid = row['DATATAPEID']
+                    timecodein = row['TIMECODEIN']
+                    folderpath = "T://DaletStorage/Video_Watch_Folder/" + row["OC_COMPONENT_NAME"]
+                    traffic_code = row['TRAFFIC_CODE'].strip("=\"")
+                    title_type = row['TITLETYPE']
+                    framerate = row['FRAMERATE']
+                    codec = row['CODEC']
+                    v_width = row['V_WIDTH']
+                    v_height = row['V_HEIGHT']
+                    duration = row['DURATION_MS']
+                    content_type = row['CONTENT_TYPE']
 
-                    xml_body = f"\
-                    <Titles> \
-                    <Title><!-- title type video -->\
-                    <key1>{guid}</key1>\
-                    <itemcode>{guid}</itemcode>\
-                    <title>{name}</title>\
-                    <NGC_NGCITitle>{name}</NGC_NGCITitle>\
-                    <NGC_DivaTapeID>{datatapeid}</NGC_DivaTapeID>\
-                    <NGC_FolderPath>{folderpath}</NGC_FolderPath>\
-                    <StartOfMaterial>{timecodein}</StartOfMaterial>\
-                    <NGC_NGCITrafficCode>{traffic_code}</NGC_NGCITrafficCode>\
-                    <titletype>{title_type}</titletype>\
-                    <NGC_ContentType>{content_type}</NGC_ContentType>\
-                    <AMFieldFromParsing_FrameRate>{framerate}</AMFieldFromParsing_FrameRate>\
-                    <AMFieldFromParsing_Codec>{codec}</AMFieldFromParsing_Codec>\
-                    <AMFieldFromParsing_Width>{v_width}</AMFieldFromParsing_Width>\
-                    <AMFieldFromParsing_Hight>{v_height}</AMFieldFromParsing_Hight>\
-                    <duration>{duration}</duration>\
-                    \
-                    <MediaInfos>\
-                    <MediaInfo>\
-                    <mediaFormatId>100099</mediaFormatId>\
-                    <mediaStorageName>G_DIVA</mediaStorageName>\
-                    <mediaStorageId>161</mediaStorageId>\
-                    <mediaFileName>{guid}</mediaFileName>\
-                    <mediaProcessStatus>Online</mediaProcessStatus>\
-                    </MediaInfo>\
-                    </MediaInfos>\
-                    </Title>\
-                    </Titles>"
+                    os.chdir(rootpath + "_xml/")
+                    xml_doc = str(guid) + '.xml'
 
-                    xmlstr = minidom.parseString(xml_body).toprettyxml(indent="   ")
+                    with open(xml_doc, mode="w", encoding='utf-8-sig') as xdoc:
 
-                    xdoc.write(xmlstr)
-                    xdoc.close()
+                        xml_body = f"\
+                        <Titles> \
+                        <Title><!-- title type video -->\
+                        <key1>{guid}</key1>\
+                        <itemcode>{guid}</itemcode>\
+                        <title>{name}</title>\
+                        <NGC_NGCITitle>{name}</NGC_NGCITitle>\
+                        <NGC_DivaTapeID>{datatapeid}</NGC_DivaTapeID>\
+                        <NGC_FolderPath>{folderpath}</NGC_FolderPath>\
+                        <StartOfMaterial>{timecodein}</StartOfMaterial>\
+                        <NGC_NGCITrafficCode>{traffic_code}</NGC_NGCITrafficCode>\
+                        <titletype>{title_type}</titletype>\
+                        <NGC_ContentType>{content_type}</NGC_ContentType>\
+                        <AMFieldFromParsing_FrameRate>{framerate}</AMFieldFromParsing_FrameRate>\
+                        <AMFieldFromParsing_Codec>{codec}</AMFieldFromParsing_Codec>\
+                        <AMFieldFromParsing_Width>{v_width}</AMFieldFromParsing_Width>\
+                        <AMFieldFromParsing_Hight>{v_height}</AMFieldFromParsing_Hight>\
+                        <duration>{duration}</duration>\
+                        \
+                        <MediaInfos>\
+                        <MediaInfo>\
+                        <mediaFormatId>100099</mediaFormatId>\
+                        <mediaStorageName>G_DIVA</mediaStorageName>\
+                        <mediaStorageId>161</mediaStorageId>\
+                        <mediaFileName>{guid}</mediaFileName>\
+                        <mediaProcessStatus>Online</mediaProcessStatus>\
+                        </MediaInfo>\
+                        </MediaInfos>\
+                        </Title>\
+                        </Titles>"
 
-                df.at[index, 'XML_CREATED'] = "1"
+                        xmlstr = minidom.parseString(xml_body).toprettyxml(indent="   ")
+
+                        xdoc.write(xmlstr)
+                        xdoc.close()
+
+                    df.at[index, 'XML_CREATED'] = "1"
+                    count += 1
 
             os.chdir(rootpath)
             xml_2_msg = f"GORILLA-DIVA XML CREATION COMPLETED"
