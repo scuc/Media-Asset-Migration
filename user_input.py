e268086b87a8c29495d635e429684fa0e2b57d05
Steven C <stevenc.github@gmail.com>
2019-08-27 15:49:28 -0400

    Added another user input to determine if the db should cross-check against files already created.

--- user_input.py
+++ user_input.py
@@ -8,14 +8,17 @@ logger = logging.getLogger(__name__)
 def get_user_input():
 
     while True:
-        xml_val = str(input(f"How many xmls / proxies are needed in this batch?  "))
+        xml_val = str(
+            input(f"How many xmls / proxies are needed in this batch?  "))
         try:
             if int(xml_val) > 10000:
-                print(f"{xml_val} is not a valid entry for the starting index, try again.")
+                print(
+                    f"{xml_val} is not a valid entry for the starting index, try again.")
                 continue
             else:
                 xml_total = xml_val
-                xml_info_msg = (f"{xml_total} selected value for xml creation.")
+                xml_info_msg = (
+                    f"{xml_total} selected value for xml creation.")
                 break
 
         except ValueError as e:
@@ -23,24 +26,22 @@ def get_user_input():
             ValueError raised for xml value: {xml_val}.\n\
             Error Message:  {str(e)} \n\
             "
-            print(f"{xml_val} is not a valid entry for the starting index, try again.")
+            print(
+                f"{xml_val} is not a valid entry for the starting index, try again.")
             logger.error(xml_excp_msg)
             continue
 
     while True:
-        get_db = str(
-            input(f"Export a new copy of the Gor DB? [Y/N]  "))
+        response2 = str(input(f"Export a new copy of the Gor DB? [Y/N]  "))
 
         try:
-            if str(get_db.upper()) in ["Y", "YES"]:
-                getnew_db = True
-            elif str(get_db.upper()) in ["N", "NO"]:
-                getnew_db = False
-            else:
-                print("Not a valid entry, please try again.")
+            response2 = yesno_rsp(response2)
+            if response2 not in [True, False]:
                 continue
-            getdb_info_msg = (f"Create a new DB export: {getnew_db}")
-            break
+            else:
+                getnew_db = response2
+                getdb_info_msg = (f"Export new DB: {getnew_db}")
+                break
 
         except ValueError as e:
             getnew_db_excp_msg = f"\n\
@@ -51,9 +52,44 @@ def get_user_input():
             logger.error(getnew_db_excp_msg)
             continue
 
+    while True:
+        response3 = str(
+            input(f"Crosscheck assets in the existing DB? [Y/N]  "))
+
+        try:
+            response3 = yesno_rsp(response3)
+            if response3 not in [True, False]: 
+                continue
+            else: 
+                crosscheck_db = response3
+                ccdb_info_msg = (f"Crosscheck DB: {getnew_db}")
+                break
+
+        except ValueError as e:
+            crosscheck_db_excp_msg = f"\n\
+            ValueError raised for the crosscheck_db value: {crosscheck_db_excp_msg}.\n\
+            Error Message:  {str(e)} \n\
+            "
+            print(f"{str(get_db)} is not a valid response, it must be Yes or No.")
+            logger.error(croscheck_db_excp_msg)
+            continue
+
     logger.info(xml_info_msg)
     logger.info(getdb_info_msg)
-    return xml_total, getnew_db
+    logger.info(ccdb_info_msg)
+    return xml_total, getnew_db, crosscheck_db
+
+
+def yesno_rsp(response): 
+    if str(response.upper()) in ["Y", "YES"]:
+        print("TRUE")
+        return True
+    elif str(response.upper()) in ["N", "NO"]:
+        print("FALSE")
+        return False
+    else:
+        print("Not a valid entry, please try again.")
+        return None
 
 
 if __name__ == '__main__':
