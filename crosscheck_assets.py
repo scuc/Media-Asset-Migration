b39b5918694b552c5d0eeab6a8b6b3fb463ee525
Steven C <stevenc.github@gmail.com>
2019-08-27 17:55:50 -0400

    Added a line break to the status messages.

--- crosscheck_assets.py
+++ crosscheck_assets.py
@@ -12,7 +12,7 @@ import database as db
 logger = logging.getLogger(__name__)
 
 
-def crosscheck_assets(table_name): 
+def crosscheck_assets(tablename): 
     """
     check db records against the full directory list of xmls. update the records as needed. 
     """
@@ -46,8 +46,9 @@ def crosscheck_assets(table_name):
             if xml_status != None: 
                 if xml_status[1] != 1:
                     index = xml_status[0]
-                    db.update_row(table_name, index, 'xml_created', 1)
-                    xml_status_msg = f" DB updated on crosscheck - \n\
+                    db.update_row(tablename, index, 'xml_created', 1)
+                    xml_status_msg = f"\n\
+                                        DB updated on crosscheck - \n\
                                         rowid: {index}, \n\
                                         guid: {guid} \n\
                                         xml_created: 1 \n"
@@ -77,8 +78,9 @@ def crosscheck_assets(table_name):
             if proxy_status != None: 
                 if proxy_status[1] != 1:
                     index = proxy_status[0]
-                    db.update_row(table_name, index, 'proxy_copied', 1)
-                    proxy_status_msg = f" DB updated on crosscheck - \n\
+                    db.update_row(tablename, index, 'proxy_copied', 1)
+                    proxy_status_msg = f"\n\
+                                        DB updated on crosscheck - \n\
                                         rowid: {index}, \n\
                                         guid: {guid} \n\
                                         proxy_copied: 1 \n"
