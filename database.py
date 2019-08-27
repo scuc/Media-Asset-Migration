f469e10bb14dc59221dd9115ba63fc1bd500dd43
Steven C <stevenc.github@gmail.com>
2019-08-27 17:55:25 -0400

    Fixed type-o in variable and sql statement.

--- database.py
+++ database.py
@@ -16,67 +16,81 @@ def connect(db_name='database.db'):
     """ Create a database connection."""
     try:
         conn = sqlite3.connect('database.db')
-    except Error as e:
+    except Exception as e:
         logger.error(e)
     return conn
 
 
-def create_table(db_name, table_name, df):
+def create_table(db_name, tablename, df):
     try:
         conn = sqlite3.connect(db_name)
-        df.to_sql(table_name, con=connect(), if_exists='replace')
-    except Error as e:
+        df.to_sql(tablename, con=connect(), if_exists='replace')
+    except Exception as e:
         logger.error(e)
     return
 
 
-def update_table(table_name, df):
+def update_table(tablename, df):
     try:
         conn = connect()
-        df.to_sql(table_name, conn, if_exists='replace', index_label='Index')
-    except Error as e:
-        # logger.error(e)
+        df.to_sql(tablename, conn, if_exists='replace', index_label='Index')
+    except Exception as e:
+        logger.error(e)
         print(e)
 
     return
 
 def select_row(index):
-    conn = connect()
-    c = conn.cursor()
-    sql = c.execute('''SELECT * FROM assets WHERE rowid=?''', (index,))
-    row = sql.fetchone()
-    print(row)
-    return sql
+    try:
+        conn = connect()
+        c = conn.cursor()
+        sql = c.execute('''SELECT * FROM assets WHERE rowid=?''', (index,))
+        row = sql.fetchone()
+        print(row)
+        return sql
+    except Exception as e: 
+        logger.error(e)
+        print(e)
 
-def update_row(table_name, index, col_name, col_val):
-    conn = connect()
-    c = conn.cursor()
-    sql = c.execute(''' UPDATE ?
-                    SET ? = ? ,,
-                    WHERE id = ?''', (table_name, col_name, col_val,index,))
-    print
-
+def update_row(tablename, index, col_name, col_val):
+    try: 
+        conn = connect()
+        c = conn.cursor()
+        sql = c.execute(''' UPDATE ?
+                        SET ? = ?,
+                        WHERE id = ?''', (tablename, col_name, col_val, index,))
+    except Exception as e: 
+        logger.error(e)
+        print(e)
 
 def fetchone_xml(guid): 
-    conn = connect()
-    c = conn.cursor()
-    sql = c.execute("""SELECT rowid, xml_created
-                         FROM assets 
-                         WHERE guid=?""", (guid,))
+    try: 
+        conn = connect()
+        c = conn.cursor()
+        sql = c.execute('''SELECT rowid, xml_created
+                            FROM assets 
+                            WHERE guid=?''', (guid,))
 
-    xml_status = c.fetchone()
-    return xml_status
+        xml_status = c.fetchone()
+        return xml_status
+    except Exception as e: 
+        logger.error(e)
+        print(e)
 
 
 def fetchone_proxy(guid): 
-    conn = connect()
-    c = conn.cursor()
-    sql = c.execute("""SELECT rowid, proxy_copied
-                         FROM assets 
-                         WHERE guid=?""", (guid,))
+    try:
+        conn = connect()
+        c = conn.cursor()
+        sql = c.execute('''SELECT rowid, proxy_copied
+                            FROM assets 
+                            WHERE guid=?''', (guid,))
 
-    proxy_status = c.fetchone()
-    return proxy_status
+        proxy_status = c.fetchone()
+        return proxy_status
+    except Exception as e: 
+        logger.error(e)
+        print(e)
 
 
 if __name__ == '__main__':
