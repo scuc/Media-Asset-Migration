70cf8ac479c631e4bd178ac53b2bdf15f4a586ac
Steven C <stevenc.github@gmail.com>
2019-08-28 16:39:58 -0400

    Fixed the remaining SQL functions and added some f-strings.

--- database.py
+++ database.py
@@ -17,6 +17,8 @@ def connect(db_name='database.db'):
     try:
         conn = sqlite3.connect('database.db')
     except Exception as e:
+        conn_err_msg = (f"Error on connection to database.db")
+        logger.error(conn_err_msg)
         logger.error(e)
     return conn
 
@@ -26,6 +28,8 @@ def create_table(db_name, tablename, df):
         conn = sqlite3.connect(db_name)
         df.to_sql(tablename, con=connect(), if_exists='replace')
     except Exception as e:
+        cr_table_err_msg = f"Error on creating new db table: {tablename}"
+        logger.error(cr_table_err_msg)
         logger.error(e)
     return
 
@@ -34,64 +38,72 @@ def update_table(tablename, df):
     try:
         conn = connect()
         df.to_sql(tablename, conn, if_exists='replace', index_label='Index')
+        return
     except Exception as e:
+        upd_table_err_msg = f"Error on updating the db table: {tablename}"
         logger.error(e)
-        print(e)
 
-    return
 
 def select_row(index):
     try:
         conn = connect()
         c = conn.cursor()
-        sql = c.execute('''SELECT * FROM assets WHERE rowid=?''', (index,))
-        row = sql.fetchone()
-        print(row)
-        return sql
+        sql = '''SELECT * FROM assets WHERE rowid = ?'''
+        params = (index,)
+        row = c.execute(sql, params).fetchone()
+        conn.close()
+        return row
     except Exception as e: 
+        sel_row_err_msg = f"Error selecting row in 'assets' at index: {index}"
+        logger.error(sel_row_err_msg)
         logger.error(e)
-        print(e)
 
-def update_row(tablename, index, col_name, col_val):
+
+def update_row(tablename, col_name, col_value, index):
     try: 
         conn = connect()
-        c = conn.cursor()
-        sql = c.execute(''' UPDATE ?
-                        SET ? = ?,
-                        WHERE id = ?''', (tablename, col_name, col_val, index,))
-    except Exception as e: 
+        cur = conn.cursor()
+        sql = f'''UPDATE {tablename} SET {col_name} = {col_value} WHERE rowid = {index}'''
+        params = (tablename, col_name, col_value, index)
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+        return
+    except Exception as e:
+        upd_row_err_msg = f"Error updating row in {tablename} at index: {index}" 
+        logger.error(upd_row_err_msg)
         logger.error(e)
-        print(e)
+
 
 def fetchone_xml(guid): 
     try: 
         conn = connect()
         c = conn.cursor()
-        sql = c.execute('''SELECT rowid, xml_created
-                            FROM assets 
-                            WHERE guid=?''', (guid,))
-
-        xml_status = c.fetchone()
+        sql = '''SELECT rowid, xml_created FROM assets WHERE guid = ?'''
+        params = (guid,)
+        xml_status = c.execute(sql, params).fetchone()
+        conn.close()
         return xml_status
     except Exception as e: 
+        fetchxml_err_msg = f"Error on fetching xml status from db for guid: {guid}"
+        logger.error(fetchxml_err_msg)
         logger.error(e)
-        print(e)
 
 
 def fetchone_proxy(guid): 
     try:
         conn = connect()
         c = conn.cursor()
-        sql = c.execute('''SELECT rowid, proxy_copied
-                            FROM assets 
-                            WHERE guid=?''', (guid,))
-
-        proxy_status = c.fetchone()
+        sql = '''SELECT rowid, proxy_copied FROM assets WHERE guid = ?'''
+        params = (guid,)
+        proxy_status = c.execute(sql, params).fetchone()
+        conn.close()
         return proxy_status
     except Exception as e: 
+        fetchprxy_err_msg = f"Error on fetching proxy status from db for guid: {guid}"
+        logger.error(fetchprxy_err_msg)
         logger.error(e)
-        print(e)
 
 
 if __name__ == '__main__':
-    fetchone_xml('00215AD34D20-8000FFFF-FFFF-F2AD-A668')
+    update_row('assets','xml_created', 0, 0)
