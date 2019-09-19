85e86087d78af84186d198667ff3c99489a59ce7
Steven C <stevenc.github@gmail.com>
2019-09-19 11:45:04 -0400

    Added conditions for db updates, inserts, drops, and counts for each of them.

--- update_db.py
+++ update_db.py
@@ -18,9 +18,7 @@ def update_db(date):
     """
 
     config = cfg.get_config()
-
     rootpath = config['paths']['rootpath']
-
     clean_csv = date + "_" + "gor_diva_merged_cleaned.csv"
 
     if os.path.isfile(os.join(rootpath, 'database.db')) is not True: 
@@ -39,6 +37,16 @@ def update_db(date):
                 pd_reader = pd.read_csv(m_csv, header=0)
                 df = pd.DataFrame(pd_reader)
 
+                update_count = 0
+                update_index = []
+                drop_count = 0 
+                drop_index = []
+                insert_count = 0 
+                insert_index = []
+                mismatch_count = 0
+                mismatch_index = []
+                total_count = 0
+
                 for index, row in df.iterrows():
 
                     guid = str(row['GUID'])
@@ -51,38 +59,60 @@ def update_db(date):
                     db_aoid = db_row[24]
 
                     if (guid == db_row[1]
-                        and db_datatapeid == "Null"):
+                        and db_datatapeid == "Null"
+                        and db_aoid = "Null"):
                         db.update_row("assets", index, row)
+                        update_count + = 1
+                        update_index.append(index)
 
                     elif (guid != db_row[1]
                         and db.fetchone_guid(guid) == None):
-                        db.drop_row('assets', )
-                        pass
+                        db.drop_row('assets', index, guid)
+                        drop_count + = 1
+                        drop_index.append(index)
+
                     elif (db_row == None
                         and row['_merge'] == 'both'): 
                         db.insert_row(index, row)
-                        
+                        insert_count + = 1
+                        insert_index.append(index)
+
                     elif (guid != db_row[1]
                         and db.fetchone_guid(guid) != None):
                         mismatch_msg = f"Mismatch in the db update: {db_row[1]} != {guid}"
                         logger.error(mismatch_msg)
+                        mismatch_count += 1
+                        mismatch_index.append(index)
                         pass
 
-
-                    elif (guid != db_row[1] 
-                    and
-                    )
-
                     else: 
+                        nochange_msg = f"No change to {guid} at row index {index}."
+                        logger.error(update_error_msg)
+                        pass
+                    
+                    total_count + = 1
 
-                
+            update_summary_msg = f"\n\
+                                Update Count:  {update_cout}\n\
+                                Drop Count: {drop_count}\n\
+                                Insert Count: {insert_count}\n\
+                                Mismatch Count: {mismatch_count}\n\
+                                No Change Count: {total_count - (update_count + drop_count + insert_count + mismatch_count)}\n\
+                                Total Count: {total_count}\n\
+                                "
 
-
-         for row in cursor.fetchall():
-    stmt='''update Fixture set home_score = :home_score,away_score = :away_score WHERE home_team = :home_team and away_team =:away_team''' 
-    cur.execute(stmt,dict(home_score =row[1],away_score=row[2],home_team=row[0],away_team =row[3]))
-conn.commit()
-print "update fixture done";
+            index_summary_msg = f"\n\
+                            update index: {update_index}\n\
+                            drop index: {drop_index}\n\
+                            insert index: {insert_index}\n\
+                            mismatch index: {mismatch_index}\n\
+                            "
+            logger.info(update_summary_msg)
+            logger.info(update_index_msg)
+            
+        except Exception as e:
+            dbupdate_err_msg = f"Error updating the DB."
+            logger.exception(dbupdate_err_msg)
 
 
 if __name__ == '__main__':
