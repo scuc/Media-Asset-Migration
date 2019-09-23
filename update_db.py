c0565ecde461d46d2ed6ca1363ef58d708b9d387
Steven C <stevenc.github@gmail.com>
2019-09-23 17:01:14 -0400

    Added a condition to check for TITLETYPE.

--- update_db.py
+++ update_db.py
@@ -57,6 +57,7 @@ def update_db(date, tablename):
                     os.chdir(rootpath)
 
                     guid = str(row['GUID'])
+                    titletype = row['TITLETYPE']
                     print(str(index) + "    " + guid)
 
                     datatapeid = str(row['DATATAPEID'])
@@ -64,6 +65,7 @@ def update_db(date, tablename):
                     db_row = db.select_row(index)
                     db_datatapeid = db_row[4]
                     db_aoid = db_row[24]
+                    db_titletype = db_row[14]
 
                     if (guid == db_row[1]
                         and db_datatapeid == "Null"
@@ -72,19 +74,24 @@ def update_db(date, tablename):
                         update_count += 1
                         update_index.append(index)
 
-                    elif (guid != db_row[1]
+                    if (guid != db_row[1]
                         and db.fetchone_guid(guid) == None):
                         db.drop_row('assets', index, guid)
                         drop_count += 1
                         drop_index.append(index)
 
-                    elif (db_row == None
+                    if (db_row == None
                         and row['_merge'] == 'both'): 
                         db.insert_row(index, row)
                         insert_count += 1
                         insert_index.append(index)
 
-                    elif (guid != db_row[1]
+                    if (titletype != db_titletype):
+                        db.update_column("assets", 'TITLETYPE', titletype, index)
+                        update_count += 1
+                        update_index.append(index)
+
+                    if (guid != db_row[1]
                         and db.fetchone_guid(guid) != None):
                         mismatch_msg = f"Mismatch in the db update: {db_row[1]} != {guid}"
                         logger.error(mismatch_msg)
@@ -94,27 +101,27 @@ def update_db(date, tablename):
 
                     else: 
                         nochange_msg = f"No change to {guid} at row index {index}."
-                        logger.error(nochange_msg)
+                        logger.debug(nochange_msg)
                         # print(nochange_msg)
                         pass
                     
                     total_count += 1
 
             update_summary_msg = f"\n\
-                                Update Count:  {update_count}\n\
-                                Drop Count: {drop_count}\n\
-                                Insert Count: {insert_count}\n\
-                                Mismatch Count: {mismatch_count}\n\
-                                No Change Count: {total_count - (update_count + drop_count + insert_count + mismatch_count)}\n\
-                                Total Count: {total_count}\n\
-                                "
+                                    Update Count:  {update_count}\n\
+                                    Drop Count: {drop_count}\n\
+                                    Insert Count: {insert_count}\n\
+                                    Mismatch Count: {mismatch_count}\n\
+                                    No Change Count: {total_count - (update_count + drop_count + insert_count + mismatch_count)}\n\
+                                    Total Count: {total_count}\n\
+                                    "
 
             index_summary_msg = f"\n\
-                            update index: {update_index}\n\
-                            drop index: {drop_index}\n\
-                            insert index: {insert_index}\n\
-                            mismatch index: {mismatch_index}\n\
-                            "
+                                update index: {update_index}\n\
+                                drop index: {drop_index}\n\
+                                insert index: {insert_index}\n\
+                                mismatch index: {mismatch_index}\n\
+                                "
             logger.info(update_summary_msg)
             logger.info(index_summary_msg)
 
