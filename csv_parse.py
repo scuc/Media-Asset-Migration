9298fd234719cb066cc7fe38fd4590726cd9ec07
Steven C <8864661+scuc@users.noreply.github.com>
2024-04-01 12:44:15 -0400

    Updates the regex used to match the filenames and adds an explanation for each section of the regex.

--- csv_parse.py
+++ csv_parse.py
@@ -51,14 +51,45 @@ def db_parse(date, merged_csv):
                 """
                 index_count value set to 200K, arbitrary number, the value should 
                 be set higher as the DB grows.
+
+                """
+                """
+                1)  ^: This asserts the start of the string, ensuring that the pattern matches from the beginning.
+
+                2)  (?!.*(PGS|SVM|SDM)): This is a negative lookahead assertion (?!...) which ensures that the string does not contain the substrings "PGS", "SVM", or "SDM" anywhere. If any of these substrings are found, the match fails.
+
+                3)  ((?<![A-Z])|(?<=(-|_))): This part is a capturing group ( ... ) containing two alternatives joined by a logical OR |.
+
+                4)  (?<![A-Z]): This is a negative lookbehind assertion (?<!...) which ensures that the match is not preceded by an uppercase letter.
+
+                5)  (?<=(-|_)): This is a positive lookbehind assertion (?<=...) which ensures that the match is preceded by a hyphen or underscore.
+
+                6)  VM|EM|AVP|PPRO|FCP|PTS|GRFX|GFX|UHD|XDCAM|XDCAMHD|WAV|WAVS: This is a capturing group containing a list of options separated by the pipe |. It matches one of the specified options: "VM", "EM", "AVP", "PPRO", "FCP", "PTS", "GRFX", "GFX", "UHD", "XDCAM", "XDCAMHD", "WAV", or "WAVS".
+
+                7)  (?=(-|_|[1-5])?): This is a positive lookahead assertion (?=...) which ensures that the match is followed by an optional hyphen, underscore, or a digit between 1 and 5.
+
+                8)  (?![A-Z]): This is a negative lookahead assertion (?!...) which ensures that the match is not followed by an uppercase letter.
+                --------------------------------------
+                the regex pattern ensures that:
+
+                The string does not contain "PGS", "SVM", or "SDM".
+                The matched substring starts either not with a digit or an uppercase letter, or with a hyphen or underscore.
+                The matched substring is one of the specified options.
+                The matched substring is optionally followed by a hyphen, underscore, or a digit between 1 and 5.
+                The matched substring is not followed by an uppercase letter.
                 """
 
                 if index_count <= 200000:
 
+                    # em_check = re.search(
+                    #     r"((?<![0-9]|[A-Z])|(?<=(-|_)))(VM|EM|AVP|PPRO|FCP|PTS|GRFX|GFX|UHD|XDCAM|XDCAMHD)(?=(-|_|[1-5])?)(?![A-Z])",
+                    #     name,
+                    # )
                     em_check = re.search(
-                        r"((?<![0-9]|[A-Z])|(?<=(-|_)))(VM|EM|AVP|PPRO|FCP|PTS|GRFX|GFX|UHD|XDCAM|XDCAMHD)(?=(-|_|[1-5])?)(?![A-Z])",
+                        r"^(?!.*(PGS|SVM|SDM))((?<![0-9]|[A-Z])|(?<=(-|_)))VM|EM|AVP|PPRO|FCP|PTS|GRFX|GFX|UHD|XDCAM|XDCAMHD|WAV|WAVS(?=(-|_|[1-5])?)(?![A-Z])",
                         name,
                     )
+
                     qc_check = re.search(r"(?<=-|_)OUTGOING(?=[QC]?|-|_)", name)
 
                     if em_check is not None and qc_check is None and parsed_count == 0:
